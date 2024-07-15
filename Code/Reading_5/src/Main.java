8efea10aa5848e0ff18780cffd63aadc573790c1
xiaofeiChen <2485757827@qq.com>
2024-07-15 18:57:02 +0800

    Second

---
+++ Code/Reading_5/src/Main.java
@@ -0,0 +1,26 @@
+/**
+ * @author KiKidog
+ * @date 2024/7/15
+ */
+public class Main {
+    /**
+     * 版本控制
+     * 了解git
+     */
+    public static void main(String[] args) {
+        /**
+         * git 为分布式仓库 用于管理多人合作的项目仓库管理 避免造成数据丢失，文件错误等
+         * 常见名词 工作区 缓冲区  本地仓库 远程仓库
+         * 常见命令 git init 初始化仓库
+         * git diff 查看工作区 与 缓冲区 的文件区别
+         * git status 查看项目更新提交状态
+         * git add 文件  将工作区文件添加到缓存区 文件被跟踪
+         * git add . 将工作区文件全部添加到缓冲区
+         * git branch -M main 添加本地分支 main
+         * git commit -m "描述" 将 缓冲区文件提交到本地仓库
+         * git remote add origin URL 连接远程仓库origin
+         * git push -u origin main 将main分支的本地仓库文件提交到远程仓库origin分支中
+         * git pull 将远程仓库的文件与本地仓库的文件合并
+         */
+    }
+}
